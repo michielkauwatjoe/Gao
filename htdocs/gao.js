1f0c6c0538025769ff46103c90e1dfbac5ce12a6
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2016-02-22 20:59:50 +0100

    Adding JSON routine, need to host to serve HTTP.

--- htdocs/gao.js
+++ htdocs/gao.js
@@ -1,4 +1,12 @@
 angular.module('GaoApp', [])
-    .controller('GaoController', function() {
+    .controller('GaoController', function($scope,$http) {
         this.joseph = '高压球 (Joseph Kauw-A-Tjoe)'; 
+
+        $http.get('gao.json')
+                .success(function(data) {
+                    $scope.contents=data;
+                })
+        .error(function(data,status,error,config){
+            $scope.contents = [{heading:"Error",description:"Could not load json data"}];
+        });
     });  
